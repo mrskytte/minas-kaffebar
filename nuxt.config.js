4a523bf2ce4cd1c9006534171623ab6b659ea2dd
Chris <mr.skytte@me.com>
2020-12-07 21:40:23 +0100

    generate routes added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
+  generate: {
+    routes: ['/'],
+  },
 
   // Target (https://go.nuxtjs.dev/config-target)
   server: {
