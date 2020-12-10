739c813b9f7371e9494ec099aa9598157d06bac2
Chris <mr.skytte@me.com>
2020-12-10 16:37:17 +0100

    Simple footer added

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer">
+    <p class="footer-text">Come down for some hygge</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  padding: 10px 0;
+  background-color: $color-yellow;
+  width: 100vw;
+  text-align: center;
+  &-text {
+    font-family: $font-title;
+    font-size: 20px;
+    line-height: 20px;
+    margin: 0;
+  }
+}
+</style>
