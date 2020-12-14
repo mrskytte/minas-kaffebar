b88d1970aab18709359a9ce00594a450e112eecb
Chris <mr.skytte@me.com>
2020-12-14 21:38:16 +0100

    button fixed

--- components/Button.vue
+++ components/Button.vue
@@ -32,23 +32,33 @@ export default {
   font-size: 20px;
   padding: 12px 24px;
   border: 1px solid $color-black;
+  transition: transform 0.3s;
   &-text {
     display: inline-block;
     transform: translate(0.3rem, 0.3rem);
+    transition: transform 0.3s;
   }
   &:before {
     content: '';
     position: absolute;
     border: 1px solid $color-black;
-    width: 100%;
-    height: 100%;
-    left: 0.4rem;
-    top: 0.4rem;
+    width: calc(100% + 2px);
+    height: calc(100% + 2px);
+    left: 0;
+    top: 0;
+    transform: translate(0.6rem, 0.6rem);
+    transition: transform 0.3s;
   }
-  &.mousedown {
-    background-color: rgba(#fff, 0.5);
+  &.mousedown,
+  &:hover {
+    transform: translate(0.3rem, 0.3rem);
+    background-color: rgba(#fff, 0.1);
+    .btn-text {
+      transform: translate(0, 0);
+    }
     &:before {
-      background-color: rgba(#fff, 0.5);
+      transform: translate(0, 0);
+      background-color: rgba(#fff, 0.1);
     }
   }
 }
